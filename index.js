fe7f9f60985449e2fc3e46169045c9fe1cfd338d
longpham28 <44454827+longpham28@users.noreply.github.com>
2018-12-30 16:01:15 +0900

    done info

--- index.js
+++
@@ -1,9 +0,0 @@
-const menu = document.querySelector('.col-navbar');
-const colBut = document.querySelector('.col-button');
-
-function toggleNav() {
-  if (menu.style.display === "block") menu.style.display = "none";
-  else menu.style.display = "block";
-}
-
-colBut.addEventListener('click', toggleNav);
---
+++ js/nav.js
@@ -0,0 +1,31 @@
+const nav = document.querySelector('nav');
+nav.innerHTML = `<a href="index.html" class="nav-logo">Kosoken</a>
+    <ul class="navbar">
+      <li><a class="nav-link" href="info1.html">会社案内</a></li>
+      <li><a class="nav-link" href="#">事業内容</a></li>
+      <li><a class="nav-link" href="#">弊社代表より</a></li>
+      <li><a class="nav-link" href="#">教育情報発信</a></li>
+    </ul>
+    <i class="col-button fas fa-bars"></i>
+`;
+
+const colNav = document.querySelector('.col-navbar');
+colNav.innerHTML = `<li><a class="nav-link" href="info1.html">会社案内</a></li>
+    <li><a class="nav-link" href="#">事業内容</a></li>
+    <li><a class="nav-link" href="#">弊社代表より</a></li>
+    <li><a class="nav-link" href="#">教育情報発信</a></li>
+`;
+const menu = document.querySelector('.col-navbar');
+const colBut = document.querySelector('.col-button');
+
+function toggleNav() {
+  if (menu.style.display === "block") menu.style.display = "none";
+  else menu.style.display = "block";
+}
+
+colBut.addEventListener('click', toggleNav);
+
+const footer = document.querySelector('.footer');
+footer.innerHTML = `    <p>株式会社高等教育総合研究所　Tel 03-5214-0524　Fax:03-5214-0784 E-mail info@kosoken.jp</p>
+    <p>Copyright &#169; 2008 KOSOKEN Co.,Ltd. All Rights Reserved.</p>
+`;
